685754e3e0669748539b9a3301f56e8378179640
Cristina Fernández <cristinafernandez.merced@gmail.com>
2021-02-17 21:35:37 +0100

    Latest version bottom_coverplate

--- src/bottom_coverplate.py
+++ src/bottom_coverplate.py
@@ -72,7 +72,7 @@ class bottom_coverplate(Obj3D):
     pos: FreeCAD.Vector
         position of the holder (considering ref_axis)
     """
-    def __init__(self, h_bottom = 1., d_bottom = 2., h_rail = 2., h_base = 2., wall_thick = 4., reinf_thick = 3., arduino_w = 55., arduino_d = 105., arduino_h = 5., sides = 10., bolt = 5., chmf_r = 1., axis_h = VZ, axis_d = VX, axis_w = None, pos_h = 1, pos_d = 3, pos_w = 0, pos = V0, name = ''):
+    def __init__(self, xtr_lat = 5., xtr_holes = 8., h_bottom = 1., d_bottom = 2., h_rail = 2., h_base = 2., wall_thick = 4., reinf_thick = 3., arduino_w = 55., arduino_d = 105., arduino_h = 5., sides = 10., bolt = 5., chmf_r = 1., axis_h = VZ, axis_d = VX, axis_w = None, pos_h = 1, pos_d = 3, pos_w = 0, pos = V0, name = ''):
         if axis_w is None or axis_w == V0:
            axis_w = axis_h.cross(axis_d) #vector product
         
@@ -101,7 +101,7 @@ class bottom_coverplate(Obj3D):
 
         # making the big box that will contain everything and will be cut
         self.tot_d =  arduino_d + d_bottom + 2 * sides
-        self.tot_w = arduino_w + 2 * wall_thick - 2.
+        self.tot_w = arduino_w + 2 * wall_thick - 2. + 2 * xtr_holes
         self.tot_h = h_base + arduino_h + h_bottom
 
         # definition of which axis is symmetrical
@@ -120,16 +120,25 @@ class bottom_coverplate(Obj3D):
         self.d_o[0] = V0
         self.d_o[1] = self.vec_d(sides/2.)
         self.d_o[2] = self.vec_d(sides)
-        self.d_o[3] = self.vec_d(sides + arduino_d - 3)
-        self.d_o[4] = self.vec_d(sides + arduino_d)
-        self.d_o[5] = self.vec_d(sides + arduino_d + d_bottom)
-        self.d_o[6] = self.vec_d(sides + arduino_d + d_bottom + sides/2.)
-        self.d_o[7] = self.vec_d(self.tot_d)
+        self.d_o[3] = self.vec_d(sides + xtr_lat)
+        self.d_o[4] = self.vec_d(sides + xtr_holes/2.)
+        self.d_o[5] = self.vec_d(sides + xtr_holes)
+        self.d_o[6] = self.vec_d(sides + arduino_d - 3. - xtr_lat)
+        self.d_o[7] = self.vec_d(sides + arduino_d + d_bottom - xtr_holes)
+        self.d_o[8] = self.vec_d(sides + arduino_d + d_bottom - xtr_holes/2)
+        self.d_o[9] = self.vec_d(sides + arduino_d - 3.)
+        self.d_o[10] = self.vec_d(sides + arduino_d)
+        self.d_o[11] = self.vec_d(sides + arduino_d + d_bottom)
+        self.d_o[12] = self.vec_d(sides + arduino_d + d_bottom + sides/2.)
+        self.d_o[13] = self.vec_d(self.tot_d)
 
         # position along axis_w
         self.w_o[0] = V0
-        self.w_o[1] = self.vec_w((arduino_w - 2.)/2.)
-        self.w_o[2] = self.vec_w(self.tot_w/2.)
+        self.w_o[1] = self.vec_w(-(arduino_w - 2. - 2 * xtr_lat)/2.)
+        self.w_o[2] = self.vec_w(-(arduino_w - 2.)/2.)
+        self.w_o[3] = self.vec_w(-(arduino_w + 2 * wall_thick - 2.)/2.)
+        self.w_o[4] = self.vec_w(-(arduino_w + 2 * wall_thick - 2. + xtr_holes)/2.)
+        self.w_o[5] = self.vec_w(-self.tot_w/2.)
 
         # calculates the position of the origin, and keeps it in attribute pos_o
         self.set_pos_o()
@@ -139,7 +148,24 @@ class bottom_coverplate(Obj3D):
         
         cut = []
 
-        for pt_d in (0, 5):
+        for pt_d in (0, 11):
+            shp_lat1 = fcfun.shp_box_dir(box_w = xtr_holes, box_d = sides, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 0, cd = 0, ch = 0, pos = self.get_pos_dwh(pt_d, -3, 0))
+            cut.append(shp_lat1)
+            shp_lat2 = fcfun.shp_box_dir(box_w = - xtr_holes, box_d = sides, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 0, cd = 0, ch = 0, pos = self.get_pos_dwh(pt_d, 3, 0))
+            cut.append(shp_lat2)
+
+        for pt_d in (2, 7):
+            shp_lat3 = fcfun.shp_box_dir(box_w = xtr_holes, box_d = xtr_holes, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 0, cd = 0, ch = 0, pos = self.get_pos_dwh(pt_d, -3, 1))
+            cut.append(shp_lat3)
+            shp_lat4 = fcfun.shp_box_dir(box_w = - xtr_holes, box_d = xtr_holes, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 0, cd = 0, ch = 0, pos = self.get_pos_dwh(pt_d, 3, 1))
+            cut.append(shp_lat4)
+
+        shp_lat5 = fcfun.shp_box_dir(box_w = xtr_holes, box_d = arduino_d + d_bottom - 2 * xtr_holes, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 0, cd = 0, ch = 0, pos = self.get_pos_dwh(5, -3, 0))
+        cut.append(shp_lat5)
+        shp_lat6 = fcfun.shp_box_dir(box_w = - xtr_holes, box_d = arduino_d + d_bottom - 2 * xtr_holes, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 0, cd = 0, ch = 0, pos = self.get_pos_dwh(5, 3, 0))
+        cut.append(shp_lat6)
+
+        for pt_d in (0, 11):
             shp_sides = fcfun.shp_box_dir(box_w = self.tot_w, box_d = sides, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 1, cd = 0, ch = 0, pos = self.get_pos_dwh(pt_d, 0, 1))
             cut.append(shp_sides)
 
@@ -152,8 +178,16 @@ class bottom_coverplate(Obj3D):
         shp_rail = fcfun.shp_box_dir(box_w = arduino_w, box_d = arduino_d, box_h = h_rail, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 1, cd = 0, ch = 0, pos = self.get_pos_dwh(2, 0, 2))
         cut.append(shp_rail)
 
-        for pt_d in (1, 6):
-            for pt_w in (-1, 1):
+        trim_mat = fcfun.shp_box_dir(box_w = arduino_w - 2. - 2 * xtr_lat, box_d = arduino_d - 3. - 2 * xtr_lat, box_h = self.tot_h, fc_axis_h = axis_h, fc_axis_d = axis_d, cw = 1, cd = 0, ch = 0, pos = self.get_pos_dwh(3, 0, 0))
+        cut.append(trim_mat)
+
+        for pt_d in (1, 12):
+            for pt_w in (-2, 2):
+                shp_hole = fcfun.shp_cylcenxtr(r = self.boltshank_r_tol, h = h_base, normal = self.axis_h, ch = 0, xtr_top = 1, xtr_bot = 1, pos = self.get_pos_dwh(pt_d, pt_w, 0)) 
+                cut.append(shp_hole)
+            
+        for pt_d in (4, 8):
+            for pt_w in (-4, 4):
                 shp_hole = fcfun.shp_cylcenxtr(r = self.boltshank_r_tol, h = h_base, normal = self.axis_h, ch = 0, xtr_top = 1, xtr_bot = 1, pos = self.get_pos_dwh(pt_d, pt_w, 0)) 
                 cut.append(shp_hole)
 
@@ -161,18 +195,26 @@ class bottom_coverplate(Obj3D):
         shp_final = shp_box.cut(shp_cut)
         doc.recompute()
 
-        for pt_d in (0, 7):
-            for pt_w in (-2, 2):
+        for pt_d in (0, 13):
+            for pt_w in (-3, 3):
                 shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(pt_d, pt_w, 0), fillet = 1, radius = chmf_r)
 
-        for pt_d in (2, 5):
-            for pt_w in (-2, 2):
-                shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(pt_d, pt_w, 1), fillet = 1, radius = chmf_r)
+        for pt_d in (2, 11):
+            for pt_w in (-3, 3):
+                shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(pt_d, pt_w, 0), fillet = 1, radius = chmf_r)
         
-        for pt_w in (-1, 1):
-            shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(4, pt_w, 3), fillet = 1, radius = chmf_r)
+        for pt_w in (-2, 2):
+            shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(10, pt_w, 3), fillet = 1, radius = chmf_r)
             for pt_h in (1, 3):
                 shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(2, pt_w, pt_h), fillet = 1, radius = chmf_r)
+
+        for pt_d in (3, 6):
+            for pt_w in (-1, 1):
+                shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(pt_d, pt_w, 0), fillet = 1, radius = 4 * chmf_r)
+
+        for pt_d in (2, 5, 7, 11):
+            for pt_w in (-5, 5):
+                shp_final = fcfun.shp_filletchamfer_dirpt(shp_final, self.axis_h, fc_pt = self.get_pos_dwh(pt_d, pt_w, 0), fillet = 1, radius = chmf_r)
         
         self.shp = shp_final
 
@@ -182,4 +224,4 @@ class bottom_coverplate(Obj3D):
         self.fco.Placement.Base = self.position
 
 doc = FreeCAD.newDocument()
-shpob_bottom = bottom_coverplate(h_bottom = 1., d_bottom = 2., h_rail = 2., h_base = 2., wall_thick = 4., reinf_thick = 3., arduino_w = 55., arduino_d = 105., arduino_h = 5., sides = 10., bolt = 4., chmf_r = 1., axis_h = VZ, axis_d = VX, axis_w = None, pos_h = 1, pos_d = 3, pos_w = 0, pos = V0)
+shpob_bottom = bottom_coverplate(xtr_lat = 5., xtr_holes = 8., h_bottom = 1., d_bottom = 2., h_rail = 2., h_base = 2., wall_thick = 4., reinf_thick = 3., arduino_w = 55., arduino_d = 105., arduino_h = 5., sides = 8., bolt = 3., chmf_r = 1., axis_h = VZ, axis_d = VX, axis_w = None, pos_h = 1, pos_d = 3, pos_w = 0, pos = V0)
